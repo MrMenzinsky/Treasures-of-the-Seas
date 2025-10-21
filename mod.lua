00282aabbe922c81af00df9ddcff30d22a83f6d8
Anton Menzinsky <17617000+Frommarn@users.noreply.github.com>
2025-10-21 13:57:08 +0200

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,7 @@
+local mod = foundation.createMod();
+
+-- Resources and their assets (icons)
+mod:dofile("scripts/resources.lua")
+
+-- Building and related functions
+mod:dofile("scripts/building.lua")
