d450ee9efd099d2e276281b04414ca8f0da04891
Anton Menzinsky <17617000+Frommarn@users.noreply.github.com>
2025-10-21 14:04:28 +0200

    Add resources (crab, shrimp, lobster) and prefab imports

--- mod.lua
+++ mod.lua
@@ -1,5 +1,38 @@
 local mod = foundation.createMod();
 
+-- Read in assets and prefabs
+-- mod:registerAssetProcessor("models/Seafood.fbx", {
+--   DataType = "BUILDING_ASSET_PROCESSOR"
+-- })
+
+-- CRABS --
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Crab_1", "PREFAB_CRAB_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Crab_2", "PREFAB_CRAB_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Crab_3", "PREFAB_CRAB_3")
+
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Crab_1", "PREFAB_SINGLE_CRAB_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Crab_2", "PREFAB_SINGLE_CRAB_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Crab_3", "PREFAB_SINGLE_CRAB_3")
+
+-- SHRIMPS --
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Shrimp_1", "PREFAB_SHRIMP_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Shrimp_2", "PREFAB_SHRIMP_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Shrimp_3", "PREFAB_SHRIMP_3")
+
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Shrimp_1", "PREFAB_SINGLE_SHRIMP_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Shrimp_2", "PREFAB_SINGLE_SHRIMP_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Shrimp_3", "PREFAB_SINGLE_SHRIMP_3")
+
+-- LOBSTERS --
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Lobster_1", "PREFAB_LOBSTER_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Lobster_2", "PREFAB_LOBSTER_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Lobster_3", "PREFAB_LOBSTER_3")
+
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Lobster_1", "PREFAB_SINGLE_LOBSTER_1")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Lobster_2", "PREFAB_SINGLE_LOBSTER_2")
+-- mod:registerAssetId("models/Seafood.fbx/Prefab/Single_Lobster_3", "PREFAB_SINGLE_LOBSTER_3")
+
+
 -- Resources and their assets (icons)
 mod:dofile("scripts/resources.lua")
 
--- scripts/resources.lua
+++ scripts/resources.lua
@@ -1,2 +1,93 @@
 local mod = ...
 
+ -- Fix custom graphics for icons
+ mod:registerAssetId("icons/crab.png", "ICON_RESOURCE_CRAB", "ATLAS_CELL")
+ mod:registerAssetId("icons/shrimp.png", "ICON_RESOURCE_SHRIMP", "ATLAS_CELL")
+ mod:registerAssetId("icons/lobster.png", "ICON_RESOURCE_LOBSTER", "ATLAS_CELL")
+ 
+ -- Existing RESOURCE_TYPE:s
+ -- FOOD (for storage?)
+ -- RESOURCE_FOOD (serfs?)
+ -- FOOD_ADVANCED (commoners?)
+ -- LUXURY_FOOD (citizens?)
+ 
+ mod:registerAsset({
+   DataType = "RESOURCE",
+   Id = "RESOURCE_CRAB",
+   ResourceName = "RESOURCE_CRAB_NAME",
+   Icon = "ICON_RESOURCE_CRAB",
+   ResourceTypeList = {
+     "FOOD",
+     "RESOURCE_FOOD"
+   },
+   ResourceLayoutType = "BASKET",
+   IsTradable = false, -- Fix trading later
+   DisplayInToolbar = true,
+   ResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_CRAB_1",
+     -- "PREFAB_CRAB_2",
+     -- "PREFAB_CRAB_3",
+   },
+   IndividualResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_SINGLE_CRAB_1",
+     -- "PREFAB_SINGLE_CRAB_2",
+     -- "PREFAB_SINGLE_CRAB_3",
+   },
+   IsOnWater = true -- I'm guessing we need this to work on water?
+ })
+ 
+ mod:registerAsset({
+   DataType = "RESOURCE",
+   Id = "RESOURCE_SHRIMP",
+   ResourceName = "RESOURCE_SHRIMP_NAME",
+   Icon = "ICON_RESOURCE_SHRIMP",
+   ResourceTypeList = {
+     "FOOD",
+     "FOOD_ADVANCED"
+   },
+   ResourceLayoutType = "BASKET",
+   IsTradable = false, -- Fix trading later
+   DisplayInToolbar = true,
+   ResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_SHRIMP_1",
+     -- "PREFAB_SHRIMP_2",
+     -- "PREFAB_SHRIMP_3",
+   },
+   IndividualResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_SINGLE_SHRIMP_1",
+     -- "PREFAB_SINGLE_SHRIMP_2",
+     -- "PREFAB_SINGLE_SHRIMP_3",
+   },
+   IsOnWater = true -- I'm guessing we need this to work on water?
+ })
+ 
+ mod:registerAsset({
+   DataType = "RESOURCE",
+   Id = "RESOURCE_LOBSTER",
+   ResourceName = "RESOURCE_LOBSTER_NAME",
+   Icon = "ICON_RESOURCE_LOBSTER",
+   ResourceTypeList = {
+     "FOOD",
+     "LUXURY_FOOD"
+   },
+   ResourceLayoutType = "BASKET",
+   IsTradable = false, -- Fix trading later
+   DisplayInToolbar = true,
+   ResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_LOBSTER_1",
+     -- "PREFAB_LOBSTER_2",
+     -- "PREFAB_LOBSTER_3",
+   },
+   IndividualResourceVisualPrefabList = {
+     "PREFAB_RESOURCE_VISUAL_FISH" -- Fix custom graphics
+     -- "PREFAB_SINGLE_LOBSTER_1",
+     -- "PREFAB_SINGLE_LOBSTER_2",
+     -- "PREFAB_SINGLE_LOBSTER_3",
+   },
+   IsOnWater = true -- I'm guessing we need this to work on water?
+ })
